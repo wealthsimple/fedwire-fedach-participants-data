a5c610429b7f04da940c1ab26a923088ab6458ce
Peter Graham <peterghm@gmail.com>
2017-03-01 18:05:05 -0500

    Parse data from fixed-width file

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+ruby '2.3.3'
+
+gem 'slither', git: 'https://github.com/ryanwood/slither'
+gem 'activesupport'
---
+++ bin/parse-and-output.rb
@@ -0,0 +1,32 @@
+require "active_support/all"
+require "slither"
+require "pp"
+
+# Format is defined at:
+# https://www.frbservices.org/EPaymentsDirectory/fedwireFormat.html
+Slither.define :fedwire_participants, by_bytes: false do |definition|
+  definition.body do |body|
+    body.trap { true }
+    body.column :routing_number, 9, type: :string
+    body.column :telegraphic_name, 18, type: :string
+    body.column :customer_name, 36, type: :string
+    body.column :state, 2, type: :string
+    body.column :city, 25, type: :string
+    body.column :funds_transfer_eligible, 1, type: :string
+    body.column :settlement_only, 1, type: :string
+    body.column :securities_transfer_eligible, 1, type: :string
+    body.column :revision_date, 8, type: :string
+  end
+end
+
+parsed = Slither.parse("./vendor/test.txt", :fedwire_participants)[:body]
+
+# Convert boolean fields from string => boolean
+parsed.map do |line|
+  line[:funds_transfer_eligible] = line[:funds_transfer_eligible] == "Y"
+  line[:settlement_only] = line[:settlement_only] == "S"
+  line[:securities_transfer_eligible] = line[:securities_transfer_eligible] == "Y"
+  line
+end
+
+pp parsed
